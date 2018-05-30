0f58236853c7aac98afa42d85401d1b186c8920e
Dormanus <39775844+Dormanus@users.noreply.github.com>
2018-05-30 20:12:13 +0200

    ajout de la classe frise dans le projet
    ajout de la frise

---
+++ Frise_chronologique-master/Projet_Tuteure/src/modele/Frise.java
@@ -0,0 +1,15 @@
+package modele;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class Frise {
+	
+	HashMap <Integer, TreeSet<Evenement> > Hash_evt;
+	String intituler;
+	
+	public Frise() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
