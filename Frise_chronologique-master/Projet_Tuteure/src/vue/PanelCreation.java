44f05d8bcc3cb595d08857ce6d2fd6773cef8fc1
Dormanus <39775844+Dormanus@users.noreply.github.com>
2018-06-03 14:10:02 +0200

    Add files via upload
    src  avec le panel cration contenat les choix pour la date et le poid

--- Frise_chronologique-master/Projet_Tuteure/src/vue/PanelCreation.java
+++ Frise_chronologique-master/Projet_Tuteure/src/vue/PanelCreation.java
@@ -1,7 +1,7 @@
 package vue;
 import java.awt.*;
 import java.util.GregorianCalendar;
-
+import java.util.Calendar;
 import javax.swing.*;
 
 import controleur.Controleur;
@@ -13,22 +13,47 @@ import modele.*;
 public class PanelCreation extends JPanel{
 	public final String NOM_BOUTON="+";
 	JButton boutonAjout = new JButton(NOM_BOUTON);	
-	Date dateFormulaire = new Date(30,05,2018);
+	Date dateFormulaire = new Date(2,6,2018);
 	JTextField fieldTitre = new JTextField(10);
 	JTextField fieldLieu= new JTextField(10);
 	JLabel labelDate = new JLabel(dateFormulaire.toString());
 	JTextArea area = new JTextArea("",5,10);
 	
+	//a enlever peutaitre
+	GregorianCalendar dateAuj = new GregorianCalendar ();
+	int anneeActuelle = dateAuj.get (Calendar.YEAR);
+	int totaleAnnee = (anneeActuelle - 1582)+1;
+	
+	
+	String choix_jour[] = new String [31];
+	String choix_mois[] = new String [12];
+	String choix_annee[]= new String[totaleAnnee];
 	String choix_poid[] = {"1","2","3","4","5"};
 	JLabel labelPoid = new JLabel("Poid");
-	JTextField fieldPoid= new JTextField(10);
 	JComboBox choixPoid;
-	
-	
-	JTextField fieldImage =  new JTextField(10);
+	JComboBox choixJour;
+	JComboBox choixMois;
+	JComboBox choixAnnee;
 	
 	public PanelCreation(){
+		
+		for(int jour=1;jour<=31;jour++) {
+			choix_jour[jour-1]=""+jour;
+		}
+		
+		for(int mois=1;mois<=12;mois++) {
+			choix_mois[mois-1]=""+mois;
+		}
+		int annee=1582;
+		for(int i=0;i<totaleAnnee;i++) {
+			choix_annee[i]=""+annee;
+			annee=annee+1;
+		}
+		
 		choixPoid = new JComboBox(choix_poid);
+		choixJour = new JComboBox(choix_jour);
+		choixMois = new JComboBox(choix_mois);
+		choixAnnee = new JComboBox(choix_annee);
 		setLayout (new GridBagLayout());
 		JLabel labelTitre = new JLabel("Titre");
 		JLabel labelLieu = new JLabel("Lieu");
@@ -47,13 +72,9 @@ public class PanelCreation extends JPanel{
 		contrainte.gridwidth=4;
 		add(fieldLieu, contrainte);
 		
-		contrainte.gridx=4 ; contrainte.gridy=0 ;
+		contrainte.gridx=7 ; contrainte.gridy=0 ;
 		contrainte.gridwidth=4;
 		add(boutonAjout, contrainte);
-		
-		contrainte.gridx=4 ; contrainte.gridy=4 ;
-		contrainte.gridwidth=1;
-		this.add(fieldPoid, contrainte);
 		
 		
 		
@@ -61,6 +82,18 @@ public class PanelCreation extends JPanel{
 		contrainte.gridx=2 ; contrainte.gridy=2 ;
 		contrainte.gridwidth=2;
 		this.add(labelDate, contrainte);
+		
+		contrainte.gridx=4 ; contrainte.gridy=0 ;
+		contrainte.gridwidth=1;
+		this.add(choixJour, contrainte);
+		
+		contrainte.gridx=5 ; contrainte.gridy=0 ;
+		contrainte.gridwidth=1;
+		this.add(choixMois, contrainte);
+
+		contrainte.gridx=6 ; contrainte.gridy=0 ;
+		contrainte.gridwidth=1;
+		this.add(choixAnnee, contrainte);
 		
 		//premier colone
 		contrainte.gridx=0 ; contrainte.gridy=0 ;
@@ -83,23 +116,27 @@ public class PanelCreation extends JPanel{
 		contrainte.gridwidth=1;
 		this.add(labelPoid, contrainte);
 		
+		contrainte.gridx=4 ; contrainte.gridy=4 ;
+		contrainte.gridwidth=1;
+		this.add(choixPoid, contrainte);
+		
 
 		
 		contrainte.gridx=0 ; contrainte.gridy=5 ;
 		contrainte.gridwidth=1;
 		this.add(labelDescription, contrainte);
 		
-		contrainte.gridx=1 ; contrainte.gridy=5 ;
+		contrainte.gridx=4 ; contrainte.gridy=5 ;
 		contrainte.gridwidth=0;
 		contrainte.gridheight=5;
 		this.add(area, contrainte);
 		
 		
-		
+			
 	}
 	
 	public Evenement getEvenement(){
-		return new Evenement(getDate(),fieldTitre.getText(),fieldLieu.getText(), 0, fieldImage.getText());
+		return new Evenement(getDate(),fieldTitre.getText(),area.getText(), 0);
 	}
 	public Date getDate(){
 		return	dateFormulaire;
@@ -124,68 +161,3 @@ public class PanelCreation extends JPanel{
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*package vue;
-
-import java.awt.GridBagConstraints;
-import java.awt.GridLayout;
-import java.awt.Insets;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
-public class PanelCreation extends JPanel{
-	//Date dateFormulaire = new Date();
-	public final String NOM_BOUTON="+";
-	JButton boutonAjout = new JButton(NOM_BOUTON);
-	JTextField fieldTitre = new JTextField(10);
-	JTextArea description = new JTextArea("",5,10);
-	JTextField poid = new JTextField(10);
-	
-	public PanelCreation(){
-		setLayout (new GridLayout(4,2,8,8));
-		JLabel labelTitre = new JLabel("Titre");
-		JLabel labelDescription = new JLabel("Description");
-		JLabel labelPoid = new JLabel("Poid");
-		
-		GridBagConstraints contrainte = new GridBagConstraints();
-		contrainte.insets=new Insets(5,5,5,5);
-
-		add(labelTitre);
-		add(fieldTitre);
-		
-		add(labelDescription);
-		add(description);
-		
-		add(labelPoid);
-		add(poid);
-		
-		add(boutonAjout);
-		
-	}
-}*/
