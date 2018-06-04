0779fb1027972f2626eef9bbb984e82c109b4479
Dormanus <39775844+Dormanus@users.noreply.github.com>
2018-06-04 15:30:29 +0200

    Add files via upload
    panelle creation avec les mnémonique et nouveau positionnement des labels et autre

--- Frise_chronologique-master/Projet_Tuteure/src/vue/PanelCreation.java
+++ Frise_chronologique-master/Projet_Tuteure/src/vue/PanelCreation.java
@@ -55,24 +55,30 @@ public class PanelCreation extends JPanel{
 		setLayout (new GridBagLayout());
 		JLabel labelTitre = new JLabel("Titre");
 		JLabel labelImage = new JLabel("Nom de l'image.format");
-		JLabel labelDate = new JLabel("Date(jour/mois/année)");
-		JLabel labelFrise = new JLabel("Ajout d'une frise");
+		JLabel labelDate = new JLabel("Date");
+		JLabel labelJour = new JLabel("Jour");
+		JLabel labelMois = new JLabel("Mois");
+		JLabel labelAnnee = new JLabel("Année");
+		JLabel labelFrise = new JLabel("Nouvelle evenement");
 		JLabel labelDescription = new JLabel("Description");
 		
 		GridBagConstraints contrainte = new GridBagConstraints();
 		contrainte.insets=new Insets(5,5,5,5);
 
 		//premiere ligne
-		contrainte.gridx=4 ; contrainte.gridy=1 ;
-		contrainte.gridwidth=4;
+		contrainte.gridx=1 ; contrainte.gridy=1 ;
+		contrainte.gridwidth=2;
+		labelTitre.setLabelFor(fieldTitre);
 		add(fieldTitre, contrainte);
 		
-		contrainte.gridx=4 ; contrainte.gridy=2 ;
-		contrainte.gridwidth=4;
+		contrainte.gridx=1 ; contrainte.gridy=2 ;
+		contrainte.gridwidth=2;
+		labelImage.setLabelFor(fieldImage);
 		add(fieldImage, contrainte);
 		
-		contrainte.gridx=6 ; contrainte.gridy=0 ;
-		contrainte.gridwidth=4;
+		contrainte.gridx=1 ; contrainte.gridy=0 ;
+		contrainte.gridwidth=2;
+		labelFrise.setLabelFor(boutonAjout);
 		add(boutonAjout, contrainte);
 		boutonAjout.setActionCommand(NOM_BOUTON);
 		
@@ -86,49 +92,74 @@ public class PanelCreation extends JPanel{
 		//premier colone
 		contrainte.gridx=0 ; contrainte.gridy=0 ;
 		contrainte.gridwidth=1;
+		labelFrise.setDisplayedMnemonic('N');
 		this.add(labelFrise, contrainte);
 		
 		contrainte.gridx=0 ; contrainte.gridy=1 ;
 		contrainte.gridwidth=1;
+		labelTitre.setDisplayedMnemonic('T');
 		this.add(labelTitre, contrainte);
 		
 		contrainte.gridx=0 ; contrainte.gridy=2 ;
 		contrainte.gridwidth=1;
+		labelImage.setDisplayedMnemonic('O');
 		this.add(labelImage, contrainte);
 		
 		contrainte.gridx=0 ; contrainte.gridy=3 ;
 		contrainte.gridwidth=1;
-		this.add(labelDate, contrainte);
+		this.add(labelDate,contrainte);
 		
-		contrainte.gridx=4 ; contrainte.gridy=3 ;
+		contrainte.gridx=1 ; contrainte.gridy=3 ;
 		contrainte.gridwidth=1;
+		labelJour.setDisplayedMnemonic('J');
+		this.add(labelJour, contrainte);
+		
+		contrainte.gridx=2 ; contrainte.gridy=3 ;
+		contrainte.gridwidth=1;
+		labelMois.setDisplayedMnemonic('M');
+		this.add(labelMois, contrainte);
+		
+		contrainte.gridx=3 ; contrainte.gridy=3 ;
+		contrainte.gridwidth=1;
+		labelAnnee.setDisplayedMnemonic('A');
+		this.add(labelAnnee, contrainte);
+		
+		contrainte.gridx=1 ; contrainte.gridy=4 ;
+		contrainte.gridwidth=1;
+		labelJour.setLabelFor(choixJour);
 		this.add(choixJour, contrainte);
 		
-		contrainte.gridx=5 ; contrainte.gridy=3 ;
+		contrainte.gridx=2 ; contrainte.gridy=4 ;
 		contrainte.gridwidth=1;
+		labelMois.setLabelFor(choixMois);
 		this.add(choixMois, contrainte);
 
-		contrainte.gridx=6 ; contrainte.gridy=3 ;
+		contrainte.gridx=3; contrainte.gridy=4 ;
 		contrainte.gridwidth=1;
+		labelAnnee.setLabelFor(choixAnnee);
 		this.add(choixAnnee, contrainte);
 		
-		contrainte.gridx=0 ; contrainte.gridy=4 ;
+		contrainte.gridx=0 ; contrainte.gridy=5 ;
 		contrainte.gridwidth=1;
+		labelPoid.setDisplayedMnemonic('P');
 		this.add(labelPoid, contrainte);
 		
-		contrainte.gridx=4 ; contrainte.gridy=4 ;
+		contrainte.gridx=1 ; contrainte.gridy=5 ;
 		contrainte.gridwidth=1;
+		labelPoid.setLabelFor(choixPoid);
 		this.add(choixPoid, contrainte);
 		
 
 		
-		contrainte.gridx=0 ; contrainte.gridy=5 ;
+		contrainte.gridx=0 ; contrainte.gridy=6 ;
 		contrainte.gridwidth=1;
+		labelDescription.setDisplayedMnemonic('D');
 		this.add(labelDescription, contrainte);
 		
-		contrainte.gridx=4 ; contrainte.gridy=5 ;
+		contrainte.gridx=1 ; contrainte.gridy=6 ;
 		contrainte.gridwidth=2;
-		contrainte.gridheight=5;
+		//contrainte.gridheight=2;
+		labelDescription.setLabelFor(area);
 		this.add(area, contrainte);
 		
 		
@@ -138,7 +169,7 @@ public class PanelCreation extends JPanel{
 
 	
 	public Evenement getEvenement(){
-		return new Evenement(getDate(),fieldTitre.getText(),area.getText(), 0, "image_vide.jpg");
+		return new Evenement(getDate(),fieldTitre.getText(),area.getText(), (int)choixPoid.getSelectedItem(), "image_vide.jpg");
 	}
 	public Date getDate(){
 		return	dateFormulaire;
